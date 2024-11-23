88a75943b1e72f5165f9553dd7b88d5d06558ead
DmytroBoiarchuk <dimabwhite98@gmail.com>
2024-11-23 16:54:40 +0000

    test commit

---
+++ src/pages/MastersSettingPage/MastersSettingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MastersSettingPage():JSX.Element {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+}
+
+export default MastersSettingPage;
