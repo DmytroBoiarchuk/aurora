577f8e2e89b9934954d2e32a4141eac24dbe5555
DmytroBoiarchuk <dimabwhite98@gmail.com>
2024-12-17 20:55:46 +0000

    version log

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MastersPage from './pages/MastersPage/MastersPage';
 import NavBar from './components/NavBar/NavBar';
 
 function App(): JSX.Element {
+  console.log('verion/0.0.1 branch');
   return (
     <>
       <NavBar />
